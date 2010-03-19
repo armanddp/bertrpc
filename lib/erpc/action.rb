6114704ffbe020530698628b1e527983a0d6e081
Armand du Plessis <armanddp@agilisto.com>
2010-03-19 15:14:18 +0200

    Added support for ERPC with additional protocol field

---
+++ lib/erpc/action.rb
@@ -0,0 +1,9 @@
+module ERPC
+  class Action << BERTRPC::Action
+
+    def execute
+      bert_request = encode_ruby_request(t[@req.kind, @req.from, @mod, @fun, @args])
+      bert_response = transaction(bert_request)
+      decode_bert_response(bert_response)
+    end
+end
---
+++ lib/erpc/request.rb
@@ -0,0 +1,16 @@
+module ERPC
+  class Request
+    attr_accessor :from, :kind, :options
+
+    def initialize(svc, kind, from, options)
+      @svc = svc
+      @kind = kind
+      @from = from
+      @options = options
+    end
+
+    def method_missing(cmd, *args)
+      Action.new(@svc, self, cmd)
+    end
+  end
+end
